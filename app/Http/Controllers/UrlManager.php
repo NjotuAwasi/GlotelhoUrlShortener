612abb874ce7308f5dd5979a7aec122379cbafd3
Njotu Awasi <njotuawasi@gmail.com>
2025-07-10 03:07:11 +0100

    added a generate shortcode function
    - added a function(generateshortcode) in the URL model to generate a short code of 6 characters
    - added a response body in the URL controller to return the response with the short url

--- app/Http/Controllers/UrlManager.php
+++ app/Http/Controllers/UrlManager.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Url;
 use Illuminate\Http\Request;
 
 class UrlManager extends Controller
 {
-    //
+    //this function is used to create the short URL
+    function createShortUrl(Request $request){
+        //original url is required
+        $request->validate(['original_url'=>'required']);
+
+        //this line is to check if the url actually exists and gives the result to the variable $url
+        $url = Url::where("original_url", $request->original_url)->first();
+
+        //This code block will run if the url dose not exist
+        if(!$url){
+
+            //we generete a shortcode from the "generateShortcode" function in the URL model
+            $shortCode = Url::generateShortCode();
+
+            //we create a new url object to insert data to the database
+            $url = new Url();
+
+            //we insert the original url and shortcode into a datbase row and save it
+            $url->original_url = $request->original_url;
+            $url->short_code = $shortCode;
+            $url->save();
+        }
+        return response()->json([
+            'short_url' => url(path:'/').'/'.$shortCode, 'original_url'.$url, 'short_code'.$shortCode
+        ]);
+    }
 }
--- app/Models/Url.php
+++ app/Models/Url.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Url extends Model
 {
     protected $table = 'urls';
+
+    // this function is to generate a short code for the url
+    // the short code lenght will be 6 characters
+    public static function generateShortCode(){
+        return substr(md5(uniqid(rand(), true)), 0, 6);
+    }
 }
